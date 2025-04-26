f1cfd899f7923ac882744e47f4c3c5d6b1adf878
ronaldyu001 <ronaldyu001@gmail.com>
2025-04-26 11:54:36 -0600

    Added four comparison scenarios to README.

---
+++ comparisons.py
@@ -0,0 +1,22 @@
+from MicroKernel_Implementation import *
+from Monolithic_Implementation import *
+
+
+#
+#   IPC comparisons
+#
+def Micro_IPC_Comparison():
+    print()
+
+def Mono_IPC_Comparison():
+    print()
+
+
+#
+#   System Call comparisons
+#
+def Micro_SysCall_Comparison():
+    print()
+
+def Mono_IPC_Comparison():
+    print()
---
+++ main.py
@@ -0,0 +1,22 @@
+from comparisons import *
+
+
+"""
+This file contains:
+- the implementation of the Monolithic and Microkernel OSes.
+- the implementatin of our OS comparisons.
+- this file will be visualized using a GUI.
+"""
+
+
+# main implementation
+def main():
+    # boot the Monolithic and Microkernel OSes
+    print()
+
+
+    # IPC comparisons
+
+
+if __name__ == "__main__":
+    main()
