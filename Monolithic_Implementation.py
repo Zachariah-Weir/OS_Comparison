2fbe3dee9e39462d6320bebd86ed719bb1ad8939
Zachariah-Weir <156849442+Zachariah-Weir@users.noreply.github.com>
2025-04-28 00:23:39 +0000

    Monolithic commit 2

--- Monolithic_Implementation.py
+++ Monolithic_Implementation.py
@@ -4,7 +4,6 @@ import time
 This file contains the implementation of the Microkernel class and its functions.
 """
 
-
 #
 #   Monolithic class
 #
@@ -13,14 +12,19 @@ class Monolithic:
     # constructor
     def __init__( self ):
         print( f'\nLoading Monolithic Kernel...' )
-        self.file_system = File_System(kernel=self)
+        self.file_system = File_System(kernel=self) #initialize File_System with kernel address as parameter
         self.load_services()
         self.running = True
+        if (self.running):
+            print( f'\nMonolithic Kernel loaded' )
+            self.main()
 
     def load_services(self):
-        print( f'   Loading Kernel Services...')
+        print( f'Loading Kernel Services...')
         self.file_system.load_service()
-        self
+    
+    def main(self):
+        print(f'Whatever comparisons we want run')
 
 
 
@@ -29,12 +33,11 @@ class Monolithic:
 #
 class Kernel_Service:
     def __init__(self, service_name, kernel):
-        self.service_name = service_name
+        self.service_name = service_name #requires all kernel services to provide name and kernel object address
         self.kernel = kernel
 
-    def load_service(self):
+    def load_service(self): #inherited by all child classes
         print(f'  Loading {self.service_name}...')
-
 
 #
 #   File System class
@@ -45,12 +48,12 @@ class File_System(Kernel_Service):
         self.file_dict = {} #dictionary to simulate file management
 
     def read_file(self, file_name):
-        print(f'File_System: Reading from "{file_name}"...')
-        return self.file.get(file_name, "File not found")
+        print(f'File_System: Reading from "{file_name}"...') #print file read message
+        return self.file.get(file_name, "File not found") #.get method returns value of file_name key or "file not found" default
     
     def write_file(self, file_name, file_content):
-        print(f'File_System: Writing to "{file_name}"...')
-        self.file[file_name] = file_content
+        print(f'File_System: Writing to "{file_name}"...') #print file write message
+        self.file[file_name] = file_content #add file_name (key) and file_content (value) pair to file dictionary
         return True
 
 
